f8db1036045b3be507e590bfaa48dd2288c05a29
Ramana <ramanakellampalli@gmail.com>
2018-02-21 15:00:11 -0500

    Almost done left code refactorig

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -4,13 +4,16 @@ $("ul").on("click", "li",  function () {
 $("ul").on("click", "span", function (e) {
     $(this).parent().fadeOut(500, function () {
         $(this).remove();
-    })
+    });
     e.stopPropagation();
 });
 $("input[type='text']").keypress(function (e) {
    if (e.which === 13) {
        var todoText = $(this).val();
        $(this).val("");
-       $("ul").append("<li><i class=\"fa fa-trash\"></i> " + todoText + "</li>");
+       $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
    }
+});
+$(".fa-plus").click(function () {
+    $("input[type='text']").fadeToggle();
 });
